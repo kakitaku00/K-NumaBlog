738482fad17fb3844be56985818a946558dbc040
kakitaku00 <webtrakakinuma@gmail.com>
2020-02-14 01:14:38 +0900

    fix: heading

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -43,39 +43,6 @@
 </script>
 
 <style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	/* .content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	} */
 </style>
 
 <svelte:head>
@@ -83,10 +50,11 @@
 </svelte:head>
 
 <div class="content">
-	<h1>{post.title}</h1>
-	<div class="heading">
-		{#each heading as head}
-			 <div class="heading-list"><a href={`/blog/${post.id}/#${head}`} class="heading-item">{head}</a></div>
+	<h1 class="font-semibold">{post.title}</h1>
+	<div class="mb-4">
+		<h2>目次</h2>
+		{#each heading as head, index}
+			 <div class="heading-list"><a href={`/blog/${post.id}/#${head.toLowerCase()}`} class="text-blue-500 hover:text-blue-800">{index + 1}. {head}</a></div>
 		{/each}
 	</div>
 	<div class='markdown-body'>
