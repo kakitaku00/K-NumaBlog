aa82dce02dba2dfa6d36f444638869f4f7e188d9
kakitaku00 <webtrakakinuma@gmail.com>
2020-02-16 23:53:02 +0900

    update: hover

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -20,6 +20,10 @@
 	min-height: 12rem;
 }
 
+.post:hover {
+	transform: translateY(-3px);
+}
+
 </style>
 
 <svelte:head>
@@ -34,13 +38,13 @@
 				waiting for the 'click' event -->
 		<!-- <li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li> -->
 		<!-- TODO: 二回クリックしないと目次が機能しないので#をつける -->
-		<div class="post w-full rounded overflow-hidden shadow-lg flex">
+		<a rel='prefetch' href={`/blog/${post.id}#`} class="post w-full rounded overflow-hidden shadow-md flex hover:shadow-xl transition duration-500">
 			<div class="w-1/3 h-auto bg-cover" style="background-image: url({post.image.url})">
 			</div>
 			<!-- <img class="w-full" src={post.image.url} alt="thumbnail"> -->
 			<div class="w-2/3 flex flex-col">
 				<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2"><a rel='prefetch' href={`/blog/${post.id}#`}>{post.title}</a></div>
+					<div class="font-bold text-xl mb-2">{post.title}</div>
 					<p class="truncate text-gray-500 text-sm">{post.body}</p>
 				</div>
 				<div class="px-6 py-4 mt-auto flex flex-wrap justify-between">
@@ -48,6 +52,6 @@
 					<span class="flex items-center text-gray-500 text-sm"><i class="material-icons text-sm mr-1">access_time</i>{post.createdAt.replace(/T.*Z/g, '')}</span>
 				</div>
 			</div>
-		</div>
+		</a>
 	{/each}
 </div>
