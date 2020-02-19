bc7deb2b71d93012a52ee516908c128dd0872b1c
kakitaku00 <webtrakakinuma@gmail.com>
2020-02-19 16:34:38 +0900

    fix: text

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -8,6 +8,8 @@
   let email = "";
   let body = "";
 
+  let thanks = false;
+
   function handleModal() {
     const body = document.querySelector('body')
     const modal = document.querySelector('.modal')
@@ -24,7 +26,8 @@
     }
     contactApi.post('/contact', data)
       .then(res => {
-        sapper.goto(`/contact/thanks`);
+        handleModal();
+        thanks = true;
       })
       .catch((error) => {
         console.log(error);
@@ -46,33 +49,38 @@
 <div in:fade>
   <header class="mb-8">
     <h1>お問い合わせ</h1>
-    <p>管理者・当サイトへのお問い合わせはこちらからお願いいたします。</p>
+    {#if thanks}
+      <p>お問い合わせありがとうございました。</p>
+    {:else}
+      <p>以下のフォームからお問い合わせください。</p>
+    {/if}
   </header>
-  <div class="bg-white my-4 shadow p-8 rounded">
-    <div class="mb-4">
-      <label class="block">
-        <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>お名前</span>
-        <input bind:value={name} type="text" class="form-input py-1 px-2 block w-full border" placeholder="name">
-      </label>
+  {#if !thanks}
+    <div class="bg-white my-4 shadow p-8 rounded">
+      <div class="mb-4">
+        <label class="block">
+          <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>お名前</span>
+          <input bind:value={name} type="text" class="form-input py-1 px-2 block w-full border" placeholder="name">
+        </label>
+      </div>
+      <div class="mb-4">
+        <label class="block">
+          <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>メールアドレス</span>
+          <input bind:value={email} type="email" class="form-input py-1 px-2 block w-full border" placeholder="email@example.com">
+        </label>
+      </div>
+      <div class="mb-4">
+        <label class="block">
+          <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>お問い合わせ内容</span>
+          <textarea bind:value={body} class="form-textarea py-1 px-2 block w-full border" rows="5" placeholder="テキストを入力"></textarea>
+        </label>
+      </div>
+      <div class="text-center">
+        <button on:click={handleModal} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          内容を確認
+        </button>
+      </div>
     </div>
-    <div class="mb-4">
-      <label class="block">
-        <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>メールアドレス</span>
-        <input bind:value={email} type="email" class="form-input py-1 px-2 block w-full border" placeholder="email@example.com">
-      </label>
-    </div>
-    <div class="mb-4">
-      <label class="block">
-        <span class="block text-gray-700 mb-2"><i class="fas fa-asterisk text-xs mr-1 text-red-500"></i>お問い合わせ内容</span>
-        <textarea bind:value={body} class="form-textarea py-1 px-2 block w-full border" rows="5" placeholder="テキストを入力"></textarea>
-      </label>
-    </div>
-    <div class="text-center">
-      <button on:click={handleModal} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        内容を確認
-      </button>
-    </div>
-  </div>
-
+  {/if}
   <ContactModal on:click={handleSubmit} on:toggleModal={handleModal} {name} {email} {body}/>
 </div>
