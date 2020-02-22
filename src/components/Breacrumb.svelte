f3dc1b226c8279aeab2fb82183c3c4b7190d3203
kakitaku00 <webtrakakinuma@gmail.com>
2020-02-22 22:52:48 +0900

    style

--- src/components/Breacrumb.svelte
+++ src/components/Breacrumb.svelte
@@ -19,16 +19,18 @@
 :global(::before) {
 	border-color: #333;
 }
+
+.breacrumb {
+	border-bottom: 1px solid #eaecef;
+}
 </style>
 
-<div class="mb-4">
-	<div class="breacrumb p-2 mb-1">
-		<ul class="text-xs flex">
-			<li><a href="{segment || 'home'}" class:selected='{segment === "home"}'>{segmentData[segment] || "ホーム"}</a></li>
-			<!-- TODO: 他のsapperの機能で実装できるか探す -->
-			{#if $blogTitle && $page.params !== {}}
-				<li class="flex items-center"><i class="fas fa-chevron-left mx-2"></i>{$blogTitle}</li>
-			{/if}
-		</ul>
-	</div>
+<div class="breacrumb mb-4 p-2 sticky top-0 bg-white">
+	<ul class="text-xs flex">
+		<li><a href="{segment || 'home'}" class:selected='{segment === "home"}'>{segmentData[segment] || "ホーム"}</a></li>
+		<!-- TODO: 他のsapperの機能で実装できるか探す -->
+		{#if $blogTitle && $page.params !== {}}
+			<li class="flex items-center"><i class="fas fa-chevron-left mx-2"></i>{$blogTitle}</li>
+		{/if}
+	</ul>
 </div>
