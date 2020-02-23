de389adc40de41f02ef524908565594f529a0fa0
kakitaku00 <webtrakakinuma@gmail.com>
2020-02-23 17:15:19 +0900

    spレイアウト

--- src/components/Breacrumb.svelte
+++ src/components/Breacrumb.svelte
@@ -26,11 +26,11 @@
 </style>
 
 <div class="breacrumb mb-4 p-2 sticky top-0 bg-white z-10">
-	<ul class="text-xs flex">
-		<li><a href="{segment || 'home'}" class:selected='{segment === "home"}'>{segmentData[segment] || "ホーム"}</a></li>
+	<ul class="text-xs flex items-center">
+		<li class="flex-shrink-0"><a href="{segment || 'home'}" class:selected='{segment === "home"}'>{segmentData[segment] || "ホーム"}</a></li>
 		<!-- TODO: 他のsapperの機能で実装できるか探す -->
 		{#if $blogTitle && $page.params !== {}}
-			<li class="flex items-center"><i class="fas fa-chevron-left mx-2"></i>{$blogTitle}</li>
+			<li class="flex items-center truncate"><i class="fas fa-chevron-left mx-2"></i><span class="truncate">{$blogTitle}</span></li>
 		{/if}
 	</ul>
 </div>
